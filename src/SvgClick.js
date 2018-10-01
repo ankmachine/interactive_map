5b833db08e085d9c831c3d40274c9a336904a37c
Ankit Sinha <ankit.sinha.maac@gmail.com>
2018-10-01 17:25:41 +0530

    poc kinda completed but not working

--- src/SvgClick.js
+++ src/SvgClick.js
@@ -2,18 +2,98 @@ import React from "react";
 //const my_image = require("./map.svg");
 import BlrMap from "./BlrMap";
 import seats from "./seats.json";
+import TetherComponent from "react-tether";
+
+class Avatar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loaded: false,
+      errored: false
+    };
+  }
+  render() {
+    return <div className="avatar">hey</div>;
+  }
+}
+const Card = props => (
+  <div>
+    <h3>{props.person.name}</h3>
+    <h1> working </h1>
+  </div>
+);
+class Desk extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showCard: false
+    };
+  }
+  render() {
+    return (
+      <TetherComponent
+        attachment="bottom center"
+        constraint={[
+          {
+            to: "scrollParent",
+            attachment: "together"
+          }
+        ]}
+      >
+        <path
+          onMouseOver={() => this.setState({ showCard: !this.state.showCard })}
+          onMouseOut={() => this.setState({ showCard: !this.state.showCard })}
+        />
+        <Card hover={true} person={person} />
+      </TetherComponent>
+    );
+  }
+}
 
 class SvgClick extends React.Component {
   constructor() {
     super();
+    this.state = {
+      crew: [],
+      crewFiltered: [],
+      crewIsFiltered: false
+    };
   }
+
+  fetch = () => {
+    var result = [
+      {
+        name: "ankit",
+        sg_seat_: "blr - 144 - lin"
+      },
+      {
+        name: "chandra",
+        sg_seat_: "blr - 006 - lin"
+      }
+    ];
+    this.setState({ crew: result });
+  };
 
   render() {
     return (
-      <div>hello</div>
-      // <div>
-      //     <img src={my_image} />
-      // </div>
+      <div className="map-wrapper">
+        <BlrMap>
+          {seats.map((seat, i) => (
+            <Desk
+              key={i}
+              d={seat.d}
+              id={seat.id}
+              person={this.state.crew.find(
+                person => person.sg_seat_ == seat.id
+              )}
+              showDetail={this.state.crewFiltered.some(
+                crew => crew.sg_seat_ == seat.id
+              )}
+              showDetailOnHover="true"
+            />
+          ))}
+        </BlrMap>
+      </div>
     );
   }
 }
