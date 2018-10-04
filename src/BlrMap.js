3d1b9df920c5ee1215047a5eeacba7a2720703bd
Ankit Sinha <ankit.sinha.maac@gmail.com>
2018-10-04 18:01:06 +0530

    fixing blr map

--- src/BlrMap.js
+++ src/BlrMap.js
@@ -3,14 +3,28 @@ import React from "react";
 const BlrMap = props => (
   <svg
     xmlnsXlink="http://www.w3.org/1999/xlink"
-    width={1000}
-    height={1000}
+    width={1200}
+    height={1200}
     viewBox="0 0 264.583 256.165"
     {...props}
   >
-    <g fill="gray">
-      <path d="m 138.166,19.362 h 11 V 26 H 138.75 z" />
-      <path d="m 158.75,19.362738 h 10.58334 V 25.97732 H 158.75 z" />
+    <g>
+      <rect
+        x="0"
+        y="0"
+        width="920"
+        height="920"
+        fill="#ffffff"
+        stroke="#000000"
+      />
+      <rect
+        x="720"
+        y="30"
+        width="200"
+        height="80"
+        fill="#ffffff"
+        stroke="#000000"
+      />
     </g>
     <g>{props.children}</g>
   </svg>
